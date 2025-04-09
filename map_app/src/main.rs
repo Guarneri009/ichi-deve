1943b445f613f9303f6f52fa7e46bfecde29bbdd
Guarneri009 <driftinghide@gmail.com>
2025-04-09 14:12:11 +0900

    デフォルとの地図からカスタムできるように修正

--- map_app/src/main.rs
+++ map_app/src/main.rs
@@ -3,8 +3,21 @@
 use std::{io::ErrorKind, path::PathBuf};
 
 use clap::{Parser, Subcommand};
-use maplibre::{coords::LatLon, render::settings::WgpuSettings};
-use maplibre_winit::{run_headed_map, WinitMapWindowConfig};
+use maplibre::{
+    coords::LatLon,
+    environment::OffscreenKernelConfig,
+    event_loop::EventLoop,
+    io::apc::SchedulerAsyncProcedureCall,
+    kernel::{Kernel, KernelBuilder},
+    map::Map,
+    platform::{
+        ReqwestOffscreenKernelEnvironment, http_client::ReqwestHttpClient, run_multithreaded,
+        scheduler::TokioScheduler,
+    },
+    render::{RenderPlugin, builder::RendererBuilder, settings::WgpuSettings},
+    style::Style,
+};
+use maplibre_winit::{WinitEnvironment, WinitMapWindowConfig};
 
 #[cfg(feature = "headless")]
 mod headless;
@@ -52,6 +65,65 @@ enum Commands {
     },
 }
 
+pub fn run_headed_map_custom<P>(
+    cache_path: Option<P>,
+    window_config: WinitMapWindowConfig<()>,
+    wgpu_settings: WgpuSettings,
+) where
+    P: Into<PathBuf>,
+{
+    run_multithreaded(async {
+        type Environment<S, HC, APC> =
+            WinitEnvironment<S, HC, ReqwestOffscreenKernelEnvironment, APC, ()>;
+
+        let cache_path = cache_path.map(|path| path.into());
+        let client = ReqwestHttpClient::new(cache_path.clone());
+
+        let kernel: Kernel<Environment<_, _, _>> = KernelBuilder::new()
+            .with_map_window_config(window_config)
+            .with_http_client(client.clone())
+            .with_apc(SchedulerAsyncProcedureCall::new(
+                TokioScheduler::new(),
+                OffscreenKernelConfig {
+                    cache_directory: cache_path.map(|path| path.to_str().unwrap().to_string()),
+                },
+            ))
+            .with_scheduler(TokioScheduler::new())
+            .build();
+
+        let renderer_builder = RendererBuilder::new().with_wgpu_settings(wgpu_settings);
+
+        let mut map = Map::new(
+            Style::default(),
+            kernel,
+            renderer_builder,
+            vec![
+                Box::new(RenderPlugin::default()),
+                Box::new(maplibre::vector::VectorPlugin::<
+                    maplibre::vector::DefaultVectorTransferables,
+                >::default()),
+                // Box::new(maplibre::raster::RasterPlugin::<
+                //     maplibre::raster::DefaultRasterTransferables,
+                // >::default()),
+                #[cfg(debug_assertions)]
+                Box::new(maplibre::debug::DebugPlugin::default()),
+            ],
+        )
+        .unwrap();
+
+        #[cfg(not(target_os = "android"))]
+        {
+            map.initialize_renderer().await.unwrap();
+        }
+
+        map.window_mut()
+            .take_event_loop()
+            .expect("event loop is not available")
+            .run(map, None)
+            .expect("event loop creation failed")
+    })
+}
+
 fn main() {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
@@ -63,7 +135,7 @@ fn main() {
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
-        Commands::Headed {} => run_headed_map(
+        Commands::Headed {} => run_headed_map_custom(
             Some(PathBuf::from("./maplibre-cache".to_string())),
             WinitMapWindowConfig::new("maplibre".to_string()),
             WgpuSettings {
